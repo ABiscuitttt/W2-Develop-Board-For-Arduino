23a18a22df68674657fc98b0fbb61cc8656accb7
Yang <109452317+ABiscuitttt@users.noreply.github.com>
2022-07-17 14:38:10 +0800

    Project Folder of Uploader

---
+++ Romload/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
